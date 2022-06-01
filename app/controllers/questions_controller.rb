41d144c054a767723971d20e5d995276d3b0fe12
Leeroy6821 <saskilon5000@gmail.com>
2022-06-01 16:13:07 +0300

    full working form questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,17 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    render plain: params
+    @question = Question.new question_params
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+  end
+end
+
+private
+
+  def question_params
+      params.require(:question).permit(:title, :body)
   end
 end
